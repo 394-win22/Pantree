0504061e6230cdb98cb211219ddb1c410911a7e6
huaxuan250 <huaxuan250@gmail.com>
2022-01-11 20:16:59 -0600

    First Page Skeleton Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { useState, useEffect } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const grocery = {
+  title: "My Kitchen",
+  foods: {
+    milk: {
+      name: "milk",
+      buyDate: "Jan 11",
+      expDate: "Jan 15",
+    },
+    apple: {
+      name: "apple",
+      buyDate: "Jan 11",
+      expDate: "Jan 16",
+    },
+  },
+};
+
+const FoodList = ({ foods }) => (
+  <div className="food-list">
+    {Object.values(foods).map((food) => (
+      <Food food={food} />
+    ))}
+  </div>
+);
+
+const Food = ({ food }) => (
+  <div className="card m-1 p-2">
+    <div className="card-body">
+      <div className="card-title">{food.name}</div>
+      <div className="card-text">{food.buyDate}</div>
+      <div className="card-text">{food.expDate}</div>
     </div>
-  );
-}
+  </div>
+);
+
+const AddButton = () => (
+  <>
+    <button type="button" onClick={() => alert("double click")}>
+      Click Me
+    </button>
+  </>
+);
+
+const App = () => (
+  <div>
+    <h1>{grocery.title}</h1>
+    <FoodList foods={grocery.foods} />
+    <AddButton />
+  </div>
+);
 
 export default App;
