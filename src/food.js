8655954ba749b3d9193dd40fe86f0030214f0924
huaxuan250 <huaxuan250@gmail.com>
2022-01-24 10:13:09 -0600

    Icon Selection Works with Local Data

--- src/food.js
+++ src/food.js
@@ -2,129 +2,140 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { confirm } from "react-confirm-box";
 import { useState } from "react";
-import {deleteFromFirebase, useUserState} from "./utilities/firebase.js";
+import { deleteFromFirebase, useUserState } from "./utilities/firebase.js";
 import {
-    ItemCard,
-    ItemImg,
-    ItemName,
-    PurchaseDate,
-    ExpDate,
-    DeleteButton,
-  } from "./styles/PantryStyles.js";
+  ItemCard,
+  ItemImg,
+  ItemName,
+  PurchaseDate,
+  ExpDate,
+  DeleteButton,
+} from "./styles/PantryStyles.js";
 
 import { GetPhoto } from "./utilities/firebaseStorage.js";
-import {App} from "./App";
+import { App } from "./App";
 import MilkPhoto from "./components/food.png";
-import { FaTimes, FaRegMinusSquare, FaEllipsisV,FaMinus,FaRegTimesCircle} from 'react-icons/fa';
-import {EditMyForm} from "./form.js"
+import {
+  FaTimes,
+  FaRegMinusSquare,
+  FaEllipsisV,
+  FaMinus,
+  FaRegTimesCircle,
+} from "react-icons/fa";
+import { EditMyForm } from "./form.js";
 
 //It is able to get the URL. The problem is making it Async so it can actual be displayed.
 //TODO fix PROMISE<Pending>
-let Milk = GetPhoto('milk.png');
+let Milk = GetPhoto("milk.png");
 
 console.log(Milk);
-
 
 // TODO: Find a way to sort the foods
 
 export const FoodList = ({ foods }) => {
+  if (!foods) {
+    return "";
+  }
 
-    if(!foods){
-      return "";
-    }
+  const sortedFoods = Object.values(foods).sort((a, b) => {
+    return new Date(a.expDate).getTime() - new Date(b.expDate).getTime();
+  });
 
-    const sortedFoods = Object.values(foods).sort((a, b) => {
-      return new Date(a.expDate).getTime() - new Date(b.expDate).getTime();
-    });
-  
-    // console.log(typeof sortedFoods[0].expDate);
-    return (
-      <div className="food-list">
-        {Object.values(sortedFoods).map((food, index) => (
-          <Food key={index} food={food} />
-        ))}
-      </div>
-    );
-  };
+  // console.log(typeof sortedFoods[0].expDate);
+  return (
+    <div className="food-list">
+      {Object.values(sortedFoods).map((food, index) => (
+        <Food key={index} food={food} />
+      ))}
+    </div>
+  );
+};
 
 const MouseEntered = (isShown) => {
-    if(isShown){
-      return 1
-    }
-    return 2
+  if (isShown) {
+    return 1;
   }
-  
-  
+  return 2;
+};
 
 const Food = ({ food }) => {
   const [isShown, setIsShown] = useState(false);
   const user = useUserState();
-  const c = 1
-  const options = ['delete','save'];
+  const c = 1;
+  const options = ["delete", "save"];
   const defaultOption = options[0];
-    return(
-      <ItemCard 
-          onMouseEnter={() => {setIsShown(true)}}
-          onMouseLeave={() => {setIsShown(false)}}
-          bg = {MouseEntered(isShown)}
-          onDoubleClick={() => EditButton({ food , user})}
-        >
-       <DeleteButton onClick={() => deleteButton({ food , user})}>
-          <FaTimes size={40} style={{ color: isShown ? 'gray' : 'gray', float: 'right', cursor: 'pointer' }}/>
-        </DeleteButton>
-        <ItemImg src={MilkPhoto}/>
-        <ItemName >{food.name} {' '}</ItemName>
-        <PurchaseDate >{food.buyDate}</PurchaseDate>
-        <ExpDate color={Expired(food.expDate)}>
-          {food.expDate}
-        </ExpDate> 
-      </ItemCard>
-     )
-  };
+  return (
+    <ItemCard
+      onMouseEnter={() => {
+        setIsShown(true);
+      }}
+      onMouseLeave={() => {
+        setIsShown(false);
+      }}
+      bg={MouseEntered(isShown)}
+      onDoubleClick={() => EditButton({ food, user })}
+    >
+      <DeleteButton onClick={() => deleteButton({ food, user })}>
+        <FaTimes
+          size={40}
+          style={{
+            color: isShown ? "gray" : "gray",
+            float: "right",
+            cursor: "pointer",
+          }}
+        />
+      </DeleteButton>
+      <ItemImg src={food.icon} />
+      <ItemName>{food.name} </ItemName>
+      <PurchaseDate>{food.buyDate}</PurchaseDate>
+      <ExpDate color={Expired(food.expDate)}>{food.expDate}</ExpDate>
+    </ItemCard>
+  );
+};
 
-  const deleteButton = async ({ food, user }) => {
-    
-    const result = await confirm("Are you sure?");
-    if (result) {
-      deleteFromFirebase(food, user);
-      alert("Item Deletion Done!");
-      ReactDOM.render(<App />, document.getElementById("root"));
-      return;
-    }
-  };
-
-  const EditButton = async ({food, user}) => {
-    const result = await confirm("Edit Food?");
-    if(result)
-    {
-      deleteFromFirebase(food, user);
-      ReactDOM.render(<EditMyForm date = {food.buyDate} exp = {food.expDate} n = {food.name}/>, document.getElementById("root"));
-    }
+const deleteButton = async ({ food, user }) => {
+  const result = await confirm("Are you sure?");
+  if (result) {
+    deleteFromFirebase(food, user);
+    alert("Item Deletion Done!");
+    ReactDOM.render(<App />, document.getElementById("root"));
     return;
-
-  };
-
-  // return different numbers depending on expiration status
-  const Expired = (a) => {
-    const today = new Date().toISOString().substring(0, 10);
-    const diff = numDaysBetween(new Date(a),new Date(today))
-    if(diff<0){ // expired
-      return 0
-    }
-    else if((diff<=3 && diff >=0)){ // expiring soon (within 3 days)
-      return 1
-    }
-    else{ // not gonna expire soon
-      return 2
-    }
   }
+};
 
-  
+const EditButton = async ({ food, user }) => {
+  const result = await confirm("Edit Food?");
+  if (result) {
+    deleteFromFirebase(food, user);
+    ReactDOM.render(
+      <EditMyForm date={food.buyDate} exp={food.expDate} n={food.name} />,
+      document.getElementById("root")
+    );
+  }
+  return;
+};
+
+// return different numbers depending on expiration status
+const Expired = (a) => {
+  const today = new Date().toISOString().substring(0, 10);
+  const diff = numDaysBetween(new Date(a), new Date(today));
+  if (diff < 0) {
+    // expired
+    return 0;
+  } else if (diff <= 3 && diff >= 0) {
+    // expiring soon (within 3 days)
+    return 1;
+  } else {
+    // not gonna expire soon
+    return 2;
+  }
+};
+
 // to calculate the number of days between
 // d1: expiration date for the food item
 // d2: today's date
 
-  const numDaysBetween = (d1, d2) => {
-    const diff = d1.getTime() - d2.getTime();
-    return (diff / (1000 * 60 * 60 * 24))
-  };
+const numDaysBetween = (d1, d2) => {
+  const diff = d1.getTime() - d2.getTime();
+  return diff / (1000 * 60 * 60 * 24);
+};
--- src/form.js
+++ src/form.js
@@ -1,264 +1,244 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { useState } from "react";
-import {pushToFirebase, useUserState} from "./utilities/firebase.js";
-import {App} from "./App";
+import { pushToFirebase, useUserState } from "./utilities/firebase.js";
+import { App } from "./App";
+import { MySelection, Food2url } from "./select.js";
 
 export const AddButton = () => (
-    <>
-      <button
-        type="button"
-        className="btn"
-        onClick={() =>
-          ReactDOM.render(<MyForm />, document.getElementById("root"))
-        }
-      >
-        Add Food
-      </button>
-    </>
+  <>
+    <button
+      type="button"
+      className="btn"
+      onClick={() =>
+        ReactDOM.render(<MyForm />, document.getElementById("root"))
+      }
+    >
+      Add Food
+    </button>
+  </>
+);
+
+const MyForm = (param) => {
+  var experation = new Date().toISOString().substring(0, 10);
+  var today = new Date().toISOString().substring(0, 10);
+  var na = "";
+  console.log(param.n);
+
+  const [name, setName] = useState(na);
+  const [buyDate, setbuyDate] = useState(today);
+  const [expDate, setexpDate] = useState(experation);
+  const [icon, setIcon] = useState("");
+  const [textarea, setTextarea] = useState("Please add a food item.");
+
+  const user = useUserState();
+
+  function stateChange() {
+    setTimeout(function () {
+      setTextarea("Please add the next item.");
+    }, 3000);
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if (!name || !buyDate || !expDate) {
+      alert("Please add a all Food Item data");
+      return;
+    }
+
+    if (new Date(expDate).getTime() - new Date(buyDate).getTime() < 0) {
+      alert("Please make sure the expiration date after the buy date");
+      return;
+    }
+
+    update({ icon, name, buyDate, expDate, user });
+
+    if (name != "") {
+      setTextarea("Item added!");
+    }
+
+    setName("");
+    setbuyDate(today);
+    setexpDate(today);
+    // wait several seconds and set "item added" back to "please add"
+    stateChange();
+  };
+
+  return (
+    <div className="container">
+      <form className="add-form" onSubmit={onSubmit}>
+        <div className="form-control">
+          <label>Name</label>
+          <input
+            type="text"
+            placeholder="Add Food"
+            value={name}
+            onChange={(e) =>
+              setName(
+                e.target.value.length > 10
+                  ? e.target.value.slice(0, 10) + "..."
+                  : e.target.value
+              )
+            }
+          />
+        </div>
+        <div className="form-control">
+          <label>Purchase Date</label>
+          <input
+            type="date"
+            placeholder="Purchase Date"
+            value={buyDate}
+            onChange={(e) => setbuyDate(e.target.value)}
+          />
+        </div>
+        <div className="form-control">
+          <label>Expire Date</label>
+          <input
+            type="date"
+            placeholder="Expire Date"
+            value={expDate}
+            onChange={(e) => setexpDate(e.target.value)}
+          />
+        </div>
+        <div>
+          <MySelection icon={icon} setIcon={setIcon} />
+        </div>
+        <div className="form-control">
+          <textarea value={textarea} onChange={() => setTextarea()} />
+        </div>
+        <input type="submit" value="Add item" className="btn btn-block" />
+        <input type="submit" value="Back" className="btn" onClick={back} />
+      </form>
+    </div>
   );
+};
 
-  const MyForm = (param) => {
-    
+export const EditMyForm = (param) => {
+  var experation = new Date().toISOString().substring(0, 10);
+  var today = new Date().toISOString().substring(0, 10);
+  var na = "";
+  var Editing = new Boolean(false);
 
-    var experation = new Date().toISOString().substring(0, 10);
-    var today = new Date().toISOString().substring(0, 10);
-    var na = "";
-    console.log(param.n)
-  
-    const [name, setName] = useState(na);
-    const [buyDate, setbuyDate] = useState(today);
-    const [expDate, setexpDate] = useState(experation);
-    const [textarea, setTextarea] = useState(
-      "Please add a food item."
-    );
-
-
-    const user = useUserState();
-
-
-    function stateChange() {
-      setTimeout(function () {
-        setTextarea("Please add the next item.")
-      }, 3000);
+  if (param.date !== undefined) {
+    today = param.date;
+    Editing = new Boolean(true);
   }
 
-    const onSubmit = (e) => {
-      e.preventDefault();
-
-      if (!name || !buyDate || !expDate) {
-        alert("Please add a all Food Item data");
-        return;
-      }
-  
-      if (new Date(expDate).getTime() - new Date(buyDate).getTime() < 0){
-        alert("Please make sure the expiration date after the buy date");
-        return;
-      }
-  
-      update({ name, buyDate, expDate, user});
-
-      if (name != "") {
-        setTextarea("Item added!")
-      }
-  
-      setName("");
-      setbuyDate(today);
-      setexpDate(today);
-      // wait several seconds and set "item added" back to "please add"
-      stateChange() 
-      
-    };
-
-  
-    return (
-      <div className="container">
-        <form className="add-form" onSubmit={onSubmit}>
-          <div className="form-control">
-            <label>Name</label>
-            <input
-              type="text"
-              placeholder="Add Food"
-              value={name}
-              onChange={(e) =>
-                setName(
-                  e.target.value.length > 10
-                    ? e.target.value.slice(0, 10) +'...'
-                    : e.target.value
-                )
-              }
-            />
-          </div>
-          <div className="form-control">
-            <label>Purchase Date</label>
-            <input
-              type="date"
-              placeholder="Purchase Date"
-              value={buyDate}
-              onChange={(e) => setbuyDate(e.target.value)}
-            />
-          </div>
-          <div className="form-control">
-            <label>Expire Date</label>
-            <input
-              type="date"
-              placeholder="Expire Date"
-              value={expDate}
-              onChange={(e) => setexpDate(e.target.value)}
-            />
-          </div> 
-          <div className="form-control">
-            <textarea value={textarea} onChange={()=> setTextarea()}  />
-          </div>
-          <input type="submit" value="Add item" className="btn btn-block"/>
-          <input type="submit" value="Back" className="btn" onClick={back} />
-        </form>
-      </div>
-
-      
-    );
-  };
-
-  export const EditMyForm = (param) => {
-    
-
-    var experation = new Date().toISOString().substring(0, 10);
-    var today = new Date().toISOString().substring(0, 10);
-    var na = "";
-    var Editing = new Boolean(false)
-
-
-
-    if(param.date !== undefined)
-    {
-      today = param.date;
-      Editing = new Boolean(true);
-    }
-
-    if(param.exp !== undefined)
-    {
-      experation = param.exp;
-    }
-
-    if(param.n !== undefined)
-    {
-      na = param.n;
-    }
-    
-   
-
-    console.log(param.n)
-  
-    const [name, setName] = useState(na);
-    const [buyDate, setbuyDate] = useState(today);
-    const [expDate, setexpDate] = useState(experation);
-    const [textarea, setTextarea] = useState(
-      "Please add a food item."
-    );
-
-
-    const user = useUserState();
-
-
-    function stateChange() {
-      setTimeout(function () {
-        setTextarea("Please add the next item.")
-      }, 3000);
+  if (param.exp !== undefined) {
+    experation = param.exp;
   }
 
-    const onSubmit = (e) => {
-      e.preventDefault();
+  if (param.n !== undefined) {
+    na = param.n;
+  }
 
-      if (!name || !buyDate || !expDate) {
-        alert("Please add a all Food Item data");
-        return;
-      }
-  
-      if (new Date(expDate).getTime() - new Date(buyDate).getTime() < 0){
-        alert("Please make sure the expiration date after the buy date");
-        return;
-      }
-  
-      update({ name, buyDate, expDate, user});
+  console.log(param.n);
 
-      if (name != "") {
-        setTextarea("Item added!")
-      }
-  
-      setName("");
-      setbuyDate(today);
-      setexpDate(today);
-      // wait several seconds and set "item added" back to "please add"
-      stateChange();
+  const [name, setName] = useState(na);
+  const [buyDate, setbuyDate] = useState(today);
+  const [expDate, setexpDate] = useState(experation);
+  const [icon, setIcon] = useState("");
+  const [textarea, setTextarea] = useState("Please add a food item.");
 
-      back();
-      
-    };
+  const user = useUserState();
 
-    console.log(Editing);
-  
-    return (
-      <div className="container">
-        <form className="add-form" onSubmit={onSubmit}>
-          <div className="form-control">
-            <label>Name</label>
-            <input
-              type="text"
-              placeholder="Add Food"
-              value={name}
-              onChange={(e) =>
-                setName(
-                  e.target.value.length > 10
-                    ? e.target.value.slice(0, 10) +'...'
-                    : e.target.value
-                )
-              }
-            />
-          </div>
-          <div className="form-control">
-            <label>Purchase Date</label>
-            <input
-              type="date"
-              placeholder="Purchase Date"
-              value={buyDate}
-              onChange={(e) => setbuyDate(e.target.value)}
-            />
-          </div>
-          <div className="form-control">
-            <label>Expire Date</label>
-            <input
-              type="date"
-              placeholder="Expire Date"
-              value={expDate}
-              onChange={(e) => setexpDate(e.target.value)}
-            />
-          </div> 
-          <div className="form-control">
-            <textarea value={textarea} onChange={()=> setTextarea()}  />
-          </div>
-          <input type="submit" value="Finish Editing" className="btn btn-block" />
-           
-        </form>
-      </div>
+  function stateChange() {
+    setTimeout(function () {
+      setTextarea("Please add the next item.");
+    }, 3000);
+  }
 
-      
-    );
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if (!name || !buyDate || !expDate) {
+      alert("Please add a all Food Item data");
+      return;
+    }
+
+    if (new Date(expDate).getTime() - new Date(buyDate).getTime() < 0) {
+      alert("Please make sure the expiration date after the buy date");
+      return;
+    }
+
+    update({ icon, name, buyDate, expDate, user });
+
+    if (name != "") {
+      setTextarea("Item added!");
+    }
+
+    setName("");
+    setbuyDate(today);
+    setexpDate(today);
+    // wait several seconds and set "item added" back to "please add"
+    stateChange();
+
+    back();
   };
 
-  //Update the state with new items and
-const update = ({ name, buyDate, expDate, user }) => {
-    const namex = {
-      name: name,
-      buyDate: buyDate,
-      expDate: expDate,
-    };
-  
-    const id = Math.round(Math.random() * 100000);
-    const newFood = { id, ...namex };
-    pushToFirebase(newFood, user);
+  console.log(Editing);
+
+  return (
+    <div className="container">
+      <form className="add-form" onSubmit={onSubmit}>
+        <div className="form-control">
+          <label>Name</label>
+          <input
+            type="text"
+            placeholder="Add Food"
+            value={name}
+            onChange={(e) =>
+              setName(
+                e.target.value.length > 10
+                  ? e.target.value.slice(0, 10) + "..."
+                  : e.target.value
+              )
+            }
+          />
+        </div>
+        <div className="form-control">
+          <label>Purchase Date</label>
+          <input
+            type="date"
+            placeholder="Purchase Date"
+            value={buyDate}
+            onChange={(e) => setbuyDate(e.target.value)}
+          />
+        </div>
+        <div className="form-control">
+          <label>Expire Date</label>
+          <input
+            type="date"
+            placeholder="Expire Date"
+            value={expDate}
+            onChange={(e) => setexpDate(e.target.value)}
+          />
+        </div>
+        <div className="form-control">
+          <textarea value={textarea} onChange={() => setTextarea()} />
+        </div>
+        <input type="submit" value="Finish Editing" className="btn btn-block" />
+      </form>
+    </div>
+  );
+};
+
+//Update the state with new items and
+const update = ({ icon, name, buyDate, expDate, user }) => {
+  // console.log("icon" + Food2url(icon));
+  const namex = {
+    icon: Food2url(icon),
+    name: name,
+    buyDate: buyDate,
+    expDate: expDate,
   };
-  
-  const back = () => {
-    ReactDOM.render(<App />, document.getElementById("root"));
-  };
-  
+
+  const id = Math.round(Math.random() * 100000);
+  const newFood = { id, ...namex };
+  pushToFirebase(newFood, user);
+};
+
+const back = () => {
+  ReactDOM.render(<App />, document.getElementById("root"));
+};
