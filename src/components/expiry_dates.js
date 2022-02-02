04e7713c609c3cd8e35227fce0c2c3891d2bba48
Katie Li <katie01px2019@sasstudent.org>
2022-02-01 21:01:17 -0600

    added 100 items to database

--- src/components/expiry_dates.js
+++ src/components/expiry_dates.js
@@ -44,4 +44,630 @@ export const expiry_dates =[
         fridge: "5",
         shelf: "2"
     },
+    {
+        name: "Grapes",
+        fridge: "7",
+        shelf: "1"
+    },
+    {
+        name: "Melons",
+        fridge: "4",
+        shelf: "2"
+    },
+    {
+        name: "Pears",
+        fridge: "4",
+        shelf: "5"
+    },
+    {
+        name: "Artichokes",
+        fridge: "14",
+        shelf: "2"
+    },
+    {
+        name: "Beets",
+        fridge: "10",
+        shelf: "1"
+    },
+    {
+        name: "Eggplant",
+        fridge: "4",
+        shelf: "1"
+    },
+    {
+        name: "Garlic",
+        fridge: "10",
+        shelf: "30"
+    },
+    {
+        name: "Ginger",
+        fridge: "14",
+        shelf: "2"
+    },
+    {
+        name: "Onions",
+        fridge: "60",
+        shelf: "21"
+    },
+    {
+        name: "Potatoes",
+        fridge: "14",
+        shelf: "45"
+    },
+    {
+        name: "Squash",
+        fridge: "14",
+        shelf: "7"
+    },
+    {
+        name: "Tomatoes",
+        fridge: "7",
+        shelf: "3"
+    },
+    {
+        name: "Ketchup",
+        fridge: "365",
+        shelf: "182"
+    },
+    {
+        name: "Maple Syrup",
+        fridge: "365",
+        shelf: "365"
+    },
+    {
+        name: "Mayonnaise",
+        fridge: "75",
+        shelf: "60"
+    },
+    {
+        name: "Mustard",
+        fridge: "365",
+        shelf: "30"
+    },
+    {
+        name: "Olive Oil",
+        fridge: "365",
+        shelf: "182"
+    },
+    {
+        name: "Salsa",
+        fridge: "365",
+        shelf: "30"
+    },
+    {
+        name: "Soy Sauce",
+        fridge: "1095",
+        shelf: "365"
+    },
+    {
+        name: "Vinegar",
+        fridge: "730",
+        shelf: "365"
+    },
+    {
+        name: "Rice",
+        fridge: "730",
+        shelf: "365"
+    },
+    {
+        name: "Bacon",
+        fridge: "14",
+        shelf: "2"
+    },
+    {
+        name: "Chicken",
+        fridge: "2",
+        shelf: ""
+    },
+    {
+        name: "Ground pork",
+        fridge: "2",
+        shelf: ""
+    },
+    {
+        name: "Salmon",
+        fridge: "2",
+        shelf: ""
+    },
+    {
+        name: "Tilapia",
+        fridge: "2",
+        shelf: ""
+    },
+    {
+        name: "Bass",
+        fridge: "2",
+        shelf: ""
+    },
+    {
+        name: "Pork",
+        fridge: "3",
+        shelf: ""
+    },
+    {
+        name: "Shrimp",
+        fridge: "2",
+        shelf: ""
+    },
+    {
+        name: "Shellfish",
+        fridge: "2",
+        shelf: ""
+    },
+    {
+        name: "Steak",
+        fridge: "3",
+        shelf: ""
+    },
+    {
+        name: "Mushrooms",
+        fridge: "7",
+        shelf: ""
+    },
+    {
+        name: "Raspberries",
+        fridge: "3",
+        shelf: ""
+    },
+    {
+        name: "Strawberries",
+        fridge: "3",
+        shelf: ""
+    },
+    {
+        name: "Butter",
+        fridge: "90",
+        shelf: ""
+    },
+    {
+        name: "Cream cheese",
+        fridge: "60",
+        shelf: "14"
+    },
+    {
+        name: "Eggs",
+        fridge: "35",
+        shelf: ""
+    },
+    {
+        name: "Heavy Cream",
+        fridge: "30",
+        shelf: ""
+    },
+    {
+        name: "Milk",
+        fridge: "7",
+        shelf: ""
+    },
+    {
+        name: "Sour Cream",
+        fridge: "21",
+        shelf: ""
+    },
+    {
+        name: "Tofu",
+        fridge: "21",
+        shelf: "7"
+    },
+    {
+        name: "Yogurt",
+        fridge: "10",
+        shelf: ""
+    },
+    {
+        name: "Half-and-half",
+        fridge: "4",
+        shelf: ""
+    },
+    {
+        name: "Ricotta cheese",
+        fridge: "7",
+        shelf: ""
+    },
+    {
+        name: "Cottage cheese",
+        fridge: "7",
+        shelf: ""
+    },
+    {
+        name: "Cheese",
+        fridge: "7",
+        shelf: ""
+    },
+    {
+        name: "Juice",
+        fridge: "21",
+        shelf: ""
+    },
+    {
+        name: "Orange juice",
+        fridge: "21",
+        shelf: ""
+    },
+    {
+        name: "Apple juice",
+        fridge: "21",
+        shelf: ""
+    },
+    {
+        name: "Mango juice",
+        fridge: "21",
+        shelf: ""
+    },
+    {
+        name: "Miso",
+        fridge: "90",
+        shelf: ""
+    },
+    {
+        name: "Scallops",
+        fridge: "2",
+        shelf: ""
+    },
+    {
+        name: "Squid",
+        fridge: "2",
+        shelf: ""
+    },
+    {
+        name: "Ground beef",
+        fridge: "2",
+        shelf: ""
+    },
+    {
+        name: "Lamb chop",
+        fridge: "5",
+        shelf: ""
+    },
+    {
+        name: "Lobster",
+        fridge: "2",
+        shelf: ""
+    },
+    {
+        name: "Crab",
+        fridge: "2",
+        shelf: ""
+    },
+    {
+        name: "Mussels",
+        fridge: "2",
+        shelf: ""
+    },
+    {
+        name: "Oysters",
+        fridge: "2",
+        shelf: ""
+    },
+    {
+        name: "Sausage",
+        fridge: "15",
+        shelf: ""
+    },
+    {
+        name: "Ham",
+        fridge: "7",
+        shelf: ""
+    },
+    {
+        name: "Turkey",
+        fridge: "2",
+        shelf: ""
+    },
+    {
+        name: "Chicken",
+        fridge: "2",
+        shelf: ""
+    },
+    {
+        name: "Ground Turkey",
+        fridge: "2",
+        shelf: ""
+    },
+    {
+        name: "Fried chicken",
+        fridge: "4",
+        shelf: ""
+    },
+    {
+        name: "Avocados",
+        fridge: "4",
+        shelf: "7"
+    },
+    {
+        name: "Bananas",
+        fridge: "2",
+        shelf: "7"
+    },
+    {
+        name: "Kiwi",
+        fridge: "4",
+        shelf: ""
+    },
+    {
+        name: "Papaya",
+        fridge: "7",
+        shelf: "4"
+    },
+    {
+        name: "Mangos",
+        fridge: "7",
+        shelf: "4"
+    },
+    {
+        name: "Peaches",
+        fridge: "4",
+        shelf: "7"
+    },
+    {
+        name: "Nectarines",
+        fridge: "4",
+        shelf: "7"
+    },
+    {
+        name: "Asparagus",
+        fridge: "4",
+        shelf: ""
+    },
+    {
+        name: "Bok choy",
+        fridge: "3",
+        shelf: ""
+    },
+    {
+        name: "Brussel sprouts",
+        fridge: "5",
+        shelf: ""
+    },
+    {
+        name: "Carrots",
+        fridge: "21",
+        shelf: ""
+    },
+    {
+        name: "Celery",
+        fridge: "14",
+        shelf: ""
+    },
+    {
+        name: "Corn",
+        fridge: "2",
+        shelf: ""
+    },
+    {
+        name: "Cucumbers",
+        fridge: "5",
+        shelf: ""
+    },
+    {
+        name: "Lettuce",
+        fridge: "7",
+        shelf: ""
+    },
+    {
+        name: "Mushrooms",
+        fridge: "3",
+        shelf: ""
+    },
+    {
+        name: "Okra",
+        fridge: "3",
+        shelf: ""
+    },
+    {
+        name: "Peppers",
+        fridge: "5",
+        shelf: ""
+    },
+    {
+        name: "Radishes",
+        fridge: "12",
+        shelf: ""
+    },
+    {
+        name: "Spinach",
+        fridge: "2",
+        shelf: ""
+    },
+    {
+        name: "Guacamole",
+        fridge: "4",
+        shelf: ""
+    },
+    {
+        name: "Sandwich",
+        fridge: "4",
+        shelf: ""
+    },
+    {
+        name: "Burrito",
+        fridge: "4",
+        shelf: ""
+    },
+    {
+        name: "Bread",
+        fridge: "7",
+        shelf: ""
+    },
+    {
+        name: "",
+        fridge: "",
+        shelf: ""
+    },
+    {
+        name: "",
+        fridge: "",
+        shelf: ""
+    },
+    {
+        name: "",
+        fridge: "",
+        shelf: ""
+    },
+    {
+        name: "",
+        fridge: "",
+        shelf: ""
+    },
+    {
+        name: "",
+        fridge: "",
+        shelf: ""
+    },
+    {
+        name: "",
+        fridge: "",
+        shelf: ""
+    },
+    {
+        name: "",
+        fridge: "",
+        shelf: ""
+    },
+    {
+        name: "",
+        fridge: "",
+        shelf: ""
+    },
+    {
+        name: "",
+        fridge: "",
+        shelf: ""
+    },
+    {
+        name: "",
+        fridge: "",
+        shelf: ""
+    },
+    {
+        name: "",
+        fridge: "",
+        shelf: ""
+    },
+    {
+        name: "",
+        fridge: "",
+        shelf: ""
+    },
+    {
+        name: "",
+        fridge: "",
+        shelf: ""
+    },
+    {
+        name: "",
+        fridge: "",
+        shelf: ""
+    },
+    {
+        name: "",
+        fridge: "",
+        shelf: ""
+    },
+    {
+        name: "",
+        fridge: "",
+        shelf: ""
+    },
+    {
+        name: "",
+        fridge: "",
+        shelf: ""
+    },
+    {
+        name: "",
+        fridge: "",
+        shelf: ""
+    },
+    {
+        name: "",
+        fridge: "",
+        shelf: ""
+    },
+    {
+        name: "",
+        fridge: "",
+        shelf: ""
+    },
+    {
+        name: "",
+        fridge: "",
+        shelf: ""
+    },
+    {
+        name: "",
+        fridge: "",
+        shelf: ""
+    },
+    {
+        name: "",
+        fridge: "",
+        shelf: ""
+    },
+    {
+        name: "",
+        fridge: "",
+        shelf: ""
+    },
+    {
+        name: "",
+        fridge: "",
+        shelf: ""
+    },
+    {
+        name: "",
+        fridge: "",
+        shelf: ""
+    },
+    {
+        name: "",
+        fridge: "",
+        shelf: ""
+    },
+    {
+        name: "",
+        fridge: "",
+        shelf: ""
+    },
+    {
+        name: "",
+        fridge: "",
+        shelf: ""
+    },
+    {
+        name: "",
+        fridge: "",
+        shelf: ""
+    },
+    {
+        name: "",
+        fridge: "",
+        shelf: ""
+    },
+    {
+        name: "",
+        fridge: "",
+        shelf: ""
+    },
+    {
+        name: "",
+        fridge: "",
+        shelf: ""
+    },
+    {
+        name: "",
+        fridge: "",
+        shelf: ""
+    },
+    {
+        name: "",
+        fridge: "",
+        shelf: ""
+    },
+    {
+        name: "",
+        fridge: "",
+        shelf: ""
+    },
+    
 ]
